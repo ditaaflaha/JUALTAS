afc2f86915ae06f52d0258253243f8392dbb1de6
Rama ramadani <ramapraditya37@gmail.com>
2025-03-25 23:04:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,54 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RamaController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/destinasi', function () {
+    return view('destinasi');
+});
+
+
+Route::get('/rama', [RamaController::class, 'tampilkan']);
+
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id){
+   return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+   Route::get('/dashboard', function () {
+      return 'Admin Dashboard';
+
+   });
+
+   Route::get('/users', function () {
+      return 'Admin Users';
+   });
+});
 
 Route::get('/', [HomeController::class, 'index']);
-Route ::get('/home', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
+
+//Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+   //return view('list_barang', compact('id', 'nama'));
+//});
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
